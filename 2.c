c8c5d83053f98751aa02ba1cc80cfb96e7adc739
Izabela Ramos Ferreira <izabela.head@gmail.com>
2021-06-09 04:10:30 -0300

    Exercicio de programação paralela usando openmpi
    Faça um programa que siga as seguintes orientações: a) Cada um dos 4 processos deverá ler um valor (x,y,w,z). b) O processo 0 deverá receber o valor lido pelo processo 1 e o processo 1 deverá receber o valor lido do processo 0. c) O processo 2 deverá receber o valor lido pelo processo 3 e o processo 3 deverá receber o valor lido pelo processo 2. d) O processo 0 deverá somar 1 no número recebido. e) O processo 1 deverá somar 2 no número recebido. f) O processo 2 deverá somar 3 no número recebido. g) O processo 3 deverá somar 4 no número recebido. h) O processo deverá sumarizar todos os valores dos processos 0,1,2 e 3. i) Os processos 0,1,2 e 3 deverão saber o valor do somatório adquirido na etapa h. j) O processo 0 deverá multiplicar por 2 o valor do somatório. k) O processo 1 deverá multiplicar por 3 o valor do somatório. l) O processo 2deverá multiplicar por 4 o valor do somatório. m) O processo 3 deverá multiplicar o valor do somatório por 5. n) O processo 0 deverá enviar o produto calculado para o processo 1. o) O processo 1 deverá enviar para o processo 2 o produto calculado. p) O processo 2 deverá enviar para o processo 3 o produto calculado. q) O processo 3 deverá enviar para o processo 0 o produto calculado. r) O processo 0 deverá somar 10 no valor recebido. s) o processo 1 deverá somar 11 no valor recebido. t) o processo 2 deverá somar 12 no valor recebido. u) o processo 3 deverá somar 13 no valor recebido. v) O processo 0 deverá receber os valores de todos processos, realizar a sumarização e mostrar na tela o resultado.

---
+++ 2.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <mpi.h>
+
+#include <time.h>
+
+void set_array(int * array, int tam);
+void sum(int * array, int tam);
+
+int main() {
+
+    int num_process;
+    int id_process;
+    char comunicator[MPI_MAX_PROCESSOR_NAME];
+    int name_size;
+    int * array;
+    int value;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_process);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id_process);
+    MPI_Get_processor_name(comunicator, &name_size);
+
+
+    if (id_process == 1) {
+            fprintf(stderr, "\nEnter X:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 2);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Y:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 2);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter W:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 2);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Z:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 2);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Bcast(&array[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (id_process == 0) {
+            fprintf(stderr, "\nEnter X:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 1);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Y:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 1);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter W:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 1);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Z:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 1);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Bcast(&array[1], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (id_process == 3) {
+            fprintf(stderr, "\nEnter X:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 4);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Y:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 4);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter W:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 4);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Z:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 4);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Bcast(&array[2], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (id_process == 2) {
+            fprintf(stderr, "\nEnter X:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 3);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Y:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 3);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter W:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 3);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            fprintf(stderr, "\nEnter Z:");
+            scanf("%d", &value);
+            array = (int*)calloc(value, sizeof(int));
+            set_array(array, num_process, 3);
+            MPI_Scatter(&array, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Bcast(&array[3], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (id_process == 0) {
+        sum(array, num_process*4);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+void set_array(int * array, int tam, int num) {
+    int i;
+    for (i = 0; i < tam; i++) {
+        array[i] = i + num;
+    }
+}
+
+void sum(int * array, int tam) {
+    int i, sum;
+    for(i=0; i<tam; i++) {
+        sum = sum + array[i];
+    }
+    fprintf(stderr, "\nSum %d", sum);
+}
